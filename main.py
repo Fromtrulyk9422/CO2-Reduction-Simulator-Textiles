5b417604c6deaca74ed0e5059af2028bf78d43b8
Marionl99 <147211223+Marionl99@users.noreply.github.com>
2025-02-17 01:04:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-
+import panda as pd
+data=pd.read_csv('data/textiles_data.csv')
+print("Dataset Loaded:")
+print(data)
+def calculate_total_emissions(material, weight):
+  emissions_pr_kg=data[data['Material']==material]['CO2_Emissions_kg_per_kg'].values[0]
+  return emission_per_kg*weight
+print(f"\nCalculating CO2 emissions for {weight_of_material}kg of each material:")
