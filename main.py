0f16fa31248fad8edc30356324254fe6b92e1276
Fromtrulyk9422 <147211223+Fromtrulyk9422@users.noreply.github.com>
2025-02-19 14:31:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -74,6 +74,7 @@ if st.button("Calculate CO₂ Reduction"):
                 "CO₂ Increase": f"{abs(CO2_difference):.2f}" if CO2_difference < 0 else ""})
 
     comparison_df = pd.DataFrame(comparison_data)
+    difference_column = f"Difference in CO₂ with {current_material} (kg)"
     comparison_df[difference_column] = current_material_CO2_emissions - comparison_df["CO₂ Emissions (kg)"].astype(float)
     comparison_df = comparison_df.sort_values(by=difference_column, ascending=False)
     comparison_df = comparison_df.reset_index(drop=True)
